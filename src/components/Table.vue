6cb3afa6523e9cea711607e3b3ab83bf0fc7806a
zhutianwen <2756274993@qq.com>
2019-03-12 20:31:38 +0800

    提交

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -7,7 +7,15 @@
       </el-card>
 
       <!--桌台详情对话框-->
-      <el-dialog :title="data.tid+'号桌台详情'" :visible="dialogTableDetailVisible">
+      <el-dialog :title="data.tid+'号桌台详情'" :visible="dialogTableDetailVisible" :before-close="closeDialogTableDetail">
+      <!--对话框主体-->
+         <el-tabs type="border-card" @tab-click="makeQRCode">
+           <el-tab-pane label="桌台详情">详情</el-tab-pane>
+           <el-tab-pane label="桌台二维码">
+          <img :src="qrcodeData">
+           </el-tab-pane>
+         </el-tabs> 
+      <!--对话框尾部--> 
         <div slot="footer">
            <el-button type="primary" @click="dialogTableDetailVisible=false">确定</el-button>
         </div>
@@ -19,7 +27,8 @@
 export default {
     data(){
        return{
-         dialogTableDetailVisible:false,  
+         dialogTableDetailVisible:false,
+         qrcodeData:''  
        } 
     },
   props:['data'],
@@ -33,6 +42,18 @@ export default {
       showTableDetail(){
       //console.log(this.data) 当前桌子的数据
       this.dialogTableDetailVisible=true
+      },
+      closeDialogTableDetail(){
+          this.dialogTableDetailVisible=false;
+      },
+      makeQRCode(){
+          //创建二维码--注意此方法不能再当前组件的mounted中调用，因为绘图必需的canvas在el-dialog中 对话框在组件加载时并不在dom树上
+          var qrcode=require('qrcode');
+          var tableUrl=this.$store.state.globalSettings.appUrl+'/#/'+this.data.tid;
+          //把绘制得到的图片二进制转换为字符编码
+          qrcode.toDataURL(tableUrl,{width:300,errorCorrectionLevel:'H'},(err,url)=>{
+             this.qrcodeData=url;  
+          })
       } 
   },  
   
--- src/views/DishAdd.vue
+++ src/views/DishAdd.vue
@@ -1,5 +1,56 @@
 <template>
-  <div class="">
-    <h1>DishAdd</h1>
+  <div class="xfn-dish-add">
+     <el-breadcrumb>
+       <el-breadcrumb-item to="/main">首页</el-breadcrumb-item>
+       <el-breadcrumb-item>添加菜品</el-breadcrumb-item>
+    </el-breadcrumb>
+<br><br>
+    <el-form label-width="100px">
+       <el-form-item label="菜品图片:">
+          <el-upload class="xfn-uploader" :action="uploadAction" :on-success="doUploadSucc" name="dishImg" :show-file-list="false">
+             <img v-if="imageUrl" :src="imageUrl">
+          </el-upload>
+       </el-form-item>
+    </el-form>
   </div>
 </template>
+
+<script>
+export default {
+  data(){
+    return{
+      imageUrl:'', //要显示的预览图片地址
+      uploadAction:this.$store.state.globalSettings.apiUrl+'/admin/dish/image'  //可处理文件上传的数据API
+    }
+  },
+  methods: {
+    doUploadSucc(res,file){
+      //文件上传成功后,客户端得到相应消息之后的处理函数
+      //res: 服务器端返回的响应消息
+      //file:从input[]
+      console.log(res);
+      this.imageUrl=URL.createObjectURL(file.raw);
+      //把上传的文件编码为DataURL字符串
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+  .xfn-uploader{
+    .el-upload{
+      border:1px dotted #aaa;
+      border-radius:3px;
+      cursor:pointer;
+      width:250px;
+      height:176px;
+      overflow:hidden;
+      &:hover{
+        border-color:blue;
+      }
+    }
+     img{
+      max-width:100%;
+     }
+  }
+</style>
